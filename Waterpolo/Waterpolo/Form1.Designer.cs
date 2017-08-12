7c95aac420e9ca83bcc77e780b0e58fcb7da117a
Ales Manfreda <ales.manfreda@hmhco.com>
2017-08-12 23:40:48 +0200

    Waterpolo

---
+++ Waterpolo/Waterpolo/Form1.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace Waterpolo
+{
+    partial class Waterpolo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Waterpolo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1192, 773);
+            this.Name = "Waterpolo";
+            this.Text = "Waterpolo game";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
